fd596442baa5319436f1dc460ccafb04e5b9f32e
peti <petikoch@gmail.com>
2022-02-12 18:00:36 +0100

    characterization test showing behaviour in case of number overflow

--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReport should.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReport should.kt
@@ -59,4 +59,26 @@ Total expenses: 22003
 """)
     }
 
+    @Test
+    fun `print report with number overflow for Int max value`() {
+        val testee = ExpenseReport()
+        val expenses = listOf(
+            Expense().apply { type = ExpenseType.BREAKFAST; amount = 1 },
+            Expense().apply { type = ExpenseType.BREAKFAST; amount = Int.MAX_VALUE },
+
+            Expense().apply { type = ExpenseType.CAR_RENTAL; amount = 1000 }
+        )
+
+        testee.printReport(expenses, GregorianCalendar(2022, 0, 1).time)
+
+        val capturedReport = interceptedStdout.toString()
+        assertThat(capturedReport).isEqualTo("""Expenses Sat Jan 01 00:00:00 CET 2022
+Breakfast	1	 
+Breakfast	2147483647	X
+Car Rental	1000	 
+Meal expenses: -2147483648
+Total expenses: -2147482648
+""")
+    }
+
 }
