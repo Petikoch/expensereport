f423b9ac84997eede730d4dbc23f7660b4f4314f
peti <petikoch@gmail.com>
2022-02-12 17:57:15 +0100

    initial characterization test with 100% line coverage and the meal limit corner cases

--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReport should.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReport should.kt
@@ -30,4 +30,33 @@ Total expenses: 0
 """)
     }
 
+    @Test
+    fun `print report using all types of expenses with corner cases for meal limits`() {
+        val testee = ExpenseReport()
+        val expenses = listOf(
+            Expense().apply { type = ExpenseType.DINNER; amount = 5000 },
+            Expense().apply { type = ExpenseType.DINNER; amount = 5001 },
+
+            Expense().apply { type = ExpenseType.BREAKFAST; amount = 1000 },
+            Expense().apply { type = ExpenseType.BREAKFAST; amount = 1001 },
+
+            Expense().apply { type = ExpenseType.CAR_RENTAL; amount = 1 },
+            Expense().apply { type = ExpenseType.CAR_RENTAL; amount = 10000 },
+        )
+
+        testee.printReport(expenses, GregorianCalendar(2022, 0, 1).time)
+
+        val capturedReport = interceptedStdout.toString()
+        assertThat(capturedReport).isEqualTo("""Expenses Sat Jan 01 00:00:00 CET 2022
+Dinner	5000	 
+Dinner	5001	X
+Breakfast	1000	 
+Breakfast	1001	X
+Car Rental	1	 
+Car Rental	10000	 
+Meal expenses: 12002
+Total expenses: 22003
+""")
+    }
+
 }
